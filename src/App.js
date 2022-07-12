812a9cd3172e2b1473b3b93f8882fab7db1a68f8
Ishita <ayush@theflak.in>
2022-07-12 13:13:14 +0530

    add basename

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Portfolio from "./components/Portfolio";
 function App() {
   return (
     <>
-      <Routes>
+      <Routes basename={process.env.PUBLIC_URL}>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='about' element={<About />} />
